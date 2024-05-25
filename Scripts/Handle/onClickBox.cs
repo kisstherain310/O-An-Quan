066918d225a83ac8668856ef9963fd88cd5075d8
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-05-25 21:23:06 +0700

    * bug Network

--- Scripts/Handle/onClickBox.cs
+++ Scripts/Handle/onClickBox.cs
@@ -1,17 +1,100 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Unity.Netcode;
+
+// public class onClickBox : NetworkBehaviour
+// {
+//     public int index;
+//     void OnMouseDown()
+//     {
+//         handleState();
+//     }
+
+//     void handleState()
+//     {
+//         if (IsClient)
+//         {
+//             SubmitIncreaseScoreServerRpc();
+//         }
+//         else
+//         {
+//             UpdateDirectStateClientRpc();
+//         }
+//     }
+//     public void IncreaseScore()
+//     {
+//         if (IsClient)
+//         {
+//             SubmitIncreaseScoreServerRpc();
+//         }
+//         else
+//         {
+//             UpdateDirectStateClientRpc();
+//         }
+//     }
+
+//     [ServerRpc(RequireOwnership = false)]
+//     private void SubmitIncreaseScoreServerRpc()
+//     {
+//         UpdateDirectStateClientRpc();
+//     }
+//     [ClientRpc(RequireOwnership = false)]
+//     private void UpdateDirectStateClientRpc()
+//     {
+//         if (PointModel.Ins.getValue(index) == 0) return;
+//         StateManager.Ins.getCurIndex(index);
+//         StateManager.Ins.updatePosHand(index);
+//         StateManager.Ins.showDirect(index);
+//     }
+// }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class onClickBox : MonoBehaviour
+// {
+//     public int index;
+//     void OnMouseDown()
+//     {
+//         handleState();
+//     }
+
+//     void handleState()
+//     {
+//         Debug.Log(index);
+//         if (PointModel.Ins.getValue(index) == 0) return;
+//         StateManager.Ins.getCurIndex(index);
+//         StateManager.Ins.updatePosHand(index);
+//         StateManager.Ins.showDirect(index);
+//     }
+// }
+
+
+
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class onClickBox : MonoBehaviour
+public class onClickBox : NetworkBehaviour
 {
     public int index;
     void OnMouseDown()
     {
-        handleState();
+        handleStateServerRpc();
     }
 
-    void handleState()
+
+    [ServerRpc(RequireOwnership = false)]
+    void handleStateServerRpc()
     {
+        Debug.Log(index);
         if (PointModel.Ins.getValue(index) == 0) return;
         StateManager.Ins.getCurIndex(index);
         StateManager.Ins.updatePosHand(index);
