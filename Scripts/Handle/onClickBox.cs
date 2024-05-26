fae1a0f8663cceb0a3fd301995b7571a4003ba78
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-05-26 16:15:25 +0700

    + done PvP

--- Scripts/Handle/onClickBox.cs
+++ Scripts/Handle/onClickBox.cs
@@ -38,42 +38,7 @@ public class onClickBox : NetworkBehaviour
         StateManager.Ins.getCurIndex(index);
         StateManager.Ins.updatePosHand(index);
         StateManager.Ins.showDirect(index);
-        StartCoroutine(Raida());
-    }
-    IEnumerator Raida()
-    {
-        yield return new WaitForSeconds(1);
-        if (index % 2 == 0) StateManager.Ins.setDirect("left");
-        else StateManager.Ins.setDirect("right");
     }
 }
 
-
-
-
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using Unity.Netcode;
-
-// public class onClickBox : NetworkBehaviour
-// {
-//     public int index;
-//     void OnMouseDown()
-//     {
-//         handleState();
-//     }
-
-
-//     // [ServerRpc(RequireOwnership = false)]
-//     void handleState()
-//     {
-//         Debug.Log(index);
-//         if (PointModel.Ins.getValue(index) == 0) return;
-//         StateManager.Ins.getCurIndex(index);
-//         StateManager.Ins.updatePosHand(index);
-//         StateManager.Ins.showDirect(index);
-//     }
-// }
 
