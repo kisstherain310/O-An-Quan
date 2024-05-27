2832963e3fcb0ed41284dc1d718b44997f83480b
Tran Sy Tien <laohacbacho20032003@gmail.com>
2024-05-28 01:15:18 +0700

    + hoàn thiện game

--- Scripts/router/route1toEasy.cs
+++ Scripts/router/route1toEasy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class route1toEasy : MonoBehaviour
 {
@@ -10,5 +11,6 @@ public class route1toEasy : MonoBehaviour
         UIManager.Ins.OnClose(1);
         StateManager.Ins.setTypeGame(typeGame);
         StateManager.Ins.openGamePlay();
+        NetworkManager.Singleton.StartHost();
     }
 }
--- Scripts/router/route1toMedium.cs
+++ Scripts/router/route1toMedium.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class route1toMedium : MonoBehaviour
 {
@@ -10,5 +11,6 @@ public class route1toMedium : MonoBehaviour
         UIManager.Ins.OnClose(1);
         StateManager.Ins.setTypeGame(typeGame);
         StateManager.Ins.openGamePlay();
+        NetworkManager.Singleton.StartHost();
     }
 }
